f3863ca9863a1f86ac0228c1344ec9140e88b740
Lunnaris01 <Julian_Ullrich@live.de>
2025-06-25 00:40:02 +0200

    history

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 import sys
 from dotenv import load_dotenv
+from google.genai import types
 
 if len(sys.argv)<2:
     print("Please provide the prompt: python main.py \"This is my question to the AI!\"")
     sys.exit(1)
-contents=sys.argv[1]
+user_prompt=sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,7 +21,7 @@ from google import genai
 client = genai.Client(api_key=api_key)
 
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=contents)
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(response.text)
