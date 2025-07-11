bf10cdbdb4d90a998f5e633f2e63adf28ff32f70
Julian <julian@v2202506285127358159.bestsrv.de>
2025-07-11 17:40:05 +0200

    chapter 3

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,7 @@
 import os
 
+MAX_CHARS = 10000
+
 def get_files_info(working_directory, directory=None):
 	abs_working_dir = os.path.abspath(working_directory)
 	abs_path = os.path.abspath(os.path.join(abs_working_dir,directory))
@@ -11,4 +13,20 @@ def get_files_info(working_directory, directory=None):
 	ret_str = f"Results for '{directory}' directory:\n" + "\n".join(contents)
 	print(ret_str)
 get_files_info("calculator","pkg")	
+
+
+def get_file_content(working_directory, file_path):
+	abs_working_dir = os.path.abspath(working_directory)
+	abs_filepath = os.path.abspath(os.path.join(abs_working_dir,file_path))
+	if not abs_filepath.startswith(abs_working_dir):
+		return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+	if not os.path.isfile(abs_filepath):
+		return f'Error: File not found or is not a regular file: "{file_path}"'
+	with open(abs_filepath) as file:
+		content = file.read()
+		if len(content) > MAX_CHARS:
+			return content[:10000] + f'[... File \"{file_path}\" truncated at 10000 characters]'
+		return content
+
+
 
---
+++ functions/write_file.py
@@ -0,0 +1,10 @@
+def write_files(working_directory, file_path, content):
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_filepath = os.path.abspath(os.path.join(abs_working_dir,file_path))
+        if not abs_filepath.startswith(abs_working_dir):
+                return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory' 
+        if not os.path.exists(abs_filepath):
+                os.makedirs(os.path.dirname(abs_filepath))
+        with open(abs_filepath,"w") as file:
+                file.write(content)
+                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
