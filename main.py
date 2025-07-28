4a3c907c5436905fab99092be3ae8a97d3547e69
Julian <Julian_Ullrich@live.de>
2025-07-28 09:08:54 +0200

    chapter3

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Execute Python files with optional arguments
 - Write or overwrite files
 
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+All paths you provide should be relative to the working directory. You have to always provide a directory, provide "." if you are in root. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
 
 
@@ -95,11 +95,21 @@ if verbose:
     print(f"User prompt: {user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.candidates[0].content)
 if response.function_calls is not None:
     for function_call_part in response.function_calls:
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
         result = call_function(function_call_part)
-        print(f"Result: {result.parts[0].function_response.response["result"]}")
+        print(f"Result: {result.parts[0].function_response.response['result']}")
+        messages.append(types.Content(role="model", parts=[types.Part(text=response.candidates[0].content)])) # Append the content from the model's response.
+        messages.append(types.Content(role="user", parts=[types.Part(text=result.parts[0].function_response.response["result"])])) # Append the function response
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+            )
+
 else:
     print("No function was called")
 print(response.text)
+
